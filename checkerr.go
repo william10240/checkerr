16a909c4999b9c2f2d7b4a98033676e50f9cd072
william <william@william.com>
2021-10-31 20:02:06 +0800

    111

--- checkerr.go
+++ checkerr.go
@@ -1,21 +1,17 @@
 package checkerr
 
 import (
-	"runtime"
 	"log"
+	"runtime"
 )
 
-func Checkerr(err error){
+func Checkerr(err error) {
 	if err != nil {
 		_, file, line, ok := runtime.Caller(1)
 		if ok {
-			log.SetFlags(log.LstdFlags)
 			log.Println(file, line, err.Error())
-			log.SetFlags(log.LstdFlags | log.Llongfile)
 		} else {
-			log.SetFlags(log.LstdFlags | log.Llongfile)
-			log.Println(file, line, err.Error())
-
+			log.Println(err.Error())
 		}
 	}
 }
