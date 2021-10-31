fef555fe1a199c685b61e405fdbc391134e9a1aa
william <34412175+williamyan1024@users.noreply.github.com>
2021-10-31 19:44:17 +0800

    Update checkerr.go

--- checkerr.go
+++ checkerr.go
@@ -1,7 +1,16 @@
 package checkerr
 
 func Checkerr(err error){
-	if err != nil{
-		panic(err)
+	if err != nil {
+		_, file, line, ok := runtime.Caller(1)
+		if ok {
+			log.SetFlags(log.LstdFlags)
+			log.Println(file, line, err.Error())
+			log.SetFlags(log.LstdFlags | log.Llongfile)
+		} else {
+			log.SetFlags(log.LstdFlags | log.Llongfile)
+			log.Println(file, line, err.Error())
+
+		}
 	}
 }
